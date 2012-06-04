9142694650f794d4d1c5bb29d58f206f4bc6853e
Ionut <office@ionut-titei.ro>
2012-06-04 11:58:53 -0400

    first design changes

--- application/main/views/modena_t3/includes/header_modal.php
+++ application/main/views/modena_t3/includes/header_modal.php
@@ -1,11 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>			
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
 	<title><?php echo lang('Frame')?></title>
+
 	<link rel="stylesheet" type="text/css" href="<?php echo assets_url()?>css/style.css" />
 	<script type="text/javascript" src="<?php echo assets_url()?>js/jquery-1.5.2.min.js"></script>
-	<script type="text/javascript" src="<?php echo assets_url()?>js/scripts.js"></script>	
+	<script type="text/javascript" src="<?php echo assets_url()?>js/scripts.js"></script>
 	<script type="text/javascript" src="<?php echo assets_url()?>addons/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 	<script type="text/javascript" src="<?php echo assets_url()?>addons/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-	<link rel="stylesheet" type="text/css" href="<?php echo assets_url()?>addons/fancybox/jquery.fancybox-1.3.4.css" media="screen" />		
+	<link rel="stylesheet" type="text/css" href="<?php echo assets_url()?>addons/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+
+	<link rel="stylesheet" href="<?php echo assets_url()?>css/main.css">
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
 </head>
--- application/main/views/modena_t3/template.php
+++ application/main/views/modena_t3/template.php
@@ -1,48 +1,80 @@
 <?php $this->load->view('includes/head')?>
-	<body>
-		<div id="background_img_wrap"><img src="<?php echo assets_url()?>images/bg<?php echo mt_rand(1, 3)?>.jpg" id="background-img" alt="" /></div>
-		<div id="top_menu_bg"></div>
-		<div id="warpper">
-			<div id="header">
-				<?php
-					if($this->user->id > 0){
-						$this->load->view('includes/user_account_info');
-					}else{
-						$this->load->view('includes/_top_login');
-					}						
-				?>
-
-				<div class="clear"></div>
-
-				<?php
-					if($page == 'performerMyAccount'){
-						$this->load->view('includes/menu_performer');
-					}else{
-						$this->load->view('includes/menu');
-					}
-					
-					if(isset($_categories) && $_categories && is_array($categories)){
-						$this->load->view('includes/_categories');
-					}
-										
-					$this->load->view('includes/errors');
-
-				?>
-			</div>
-			<div id="page">
-
-				<?php	
-					if(isset($_sidebar) && $_sidebar){
-						$this->load->view('includes/_sidebar');
-					}elseif(isset($page)){						
-						$this->load->view($page);
-					}
-				?>
+<body>
 
 
-			</div>
-		</div>
-		<div class="clear"></div>
-		<?php $this->load->view('includes/footer')?>
-	</body>
+<div id="site-decoration">
+    <div id="site-decoration-content">
+
+        <header id="master-header" class="clearfix">
+        	<div class="left">
+                <a id="master-logo" href="<?php echo base_url()?>"><img src="<?php echo assets_url()?>images/logo.png" alt="The Naughty And The Nice" /></a>
+                <nav id="master-nav" class="clearfix">
+                    <ul>
+                        <li class="selected"><a href="javascript:;">Home</a></li>
+                        <li><a href="javascript:;">Models</a></li>
+                        <li><a href="javascript:;">Champagne Room</a></li>
+                    </ul>
+                </nav>
+
+								<?php
+									if($page == 'performerMyAccount'){
+										$this->load->view('includes/menu_performer');
+									}else{
+										$this->load->view('includes/menu');
+									}
+
+									if(isset($_categories) && $_categories && is_array($categories)){
+										$this->load->view('includes/_categories');
+									}
+
+									$this->load->view('includes/errors');
+
+								?>
+
+            </div><!--end left-->
+            <div class="right">
+							<?php
+								if($this->user->id > 0){
+									$this->load->view('includes/user_account_info');
+								}else{
+									$this->load->view('includes/_top_login');
+								}
+							?>
+            </div><!--end right-->
+        </header><!--end master-header-->
+
+        <div class="content">
+
+            <section>
+            	<div class="">
+
+							<?php
+								if(isset($_sidebar) && $_sidebar){
+									$this->load->view('includes/_sidebar');
+								}elseif(isset($page)){
+									$this->load->view($page);
+								}
+							?>
+
+            	</div>
+            </section>
+
+        </div><!--end content-->
+
+
+
+    </div><!--end site-decoration-content-->
+</div><!--end site-decoration-->
+
+
+
+
+	<?php $this->load->view('includes/footer')?>
+	<!-- Included JS Files -->
+	<script src="<?php echo assets_url()?>javascripts/jquery.min.js"></script>
+	<script src="<?php echo assets_url()?>javascripts/modernizr.foundation.js"></script>
+	<script src="<?php echo assets_url()?>javascripts/foundation.js"></script>
+	<script src="<?php echo assets_url()?>javascripts/app.js"></script>
+
+</body>
 </html>
